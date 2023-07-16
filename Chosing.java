7d9541cc370ac91cb0258610a1d01486104c23f9
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-07-16 09:08:53 +0300

    Done

---
+++ Chosing.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Chosing extends Laptops {
+
+    public Chosing(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+ 
+}
---
+++ Laptops.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+public class Laptops {
+    int id;
+    int ram;
+    int ssd;
+    String os;
+    String color;
+
+    public Laptops (int id, int ram, int ssd, String os, String color){
+        this.id = id;
+        this.ram = ram;
+        this.ssd = ssd;
+        this.os = os;
+        this.color = color;
+    }
+
+    public String toString() {
+        return String.format("id: %d, ram: %d ГБ, ssd: %d ГБ, OS: %s, color: %s", id, ram, ssd, os, color);
+    }
+
+    public boolean equals(Object o) {
+        Laptops t = (Laptops) o;
+        return id == t.id;
+    }
+}
